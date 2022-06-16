46d71576f765e47fdbf306b13e25dfbd6894b91e
lilblockzxc <alexzyrinjr@gmail.com>
2022-06-16 15:26:41 +0300

    comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { TaskItem } from "./components/TaskItem";
+import "./App.css";
+import TaskList from "./components/TaskList";
+const App = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
 
-function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, title: "react", body: "zanimaca" },
+  ]);
+
+  const addNewTask = () => {
+    const newTask = { id: Date.now(), title, body };
+    return setTasks([...tasks, newTask]), setBody(""), setTitle("");
+  };
+  const removeTask = (task) => {
+    return setTasks(tasks.filter((x) => x.id !== task.id));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        value={title}
+        placeholder={"Название "}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      />
+      <input
+        value={body}
+        placeholder={"Описание "}
+        onChange={(e) => {
+          setBody(e.target.value);
+        }}
+      />
+      <button onClick={addNewTask}>Добавить задачу</button>
+      {tasks.length !== 0 ? (
+        <TaskList tasks={tasks} remove={removeTask} />
+      ) : (
+        <h1>Задач нет</h1>
+      )}
     </div>
   );
-}
+};
 
 export default App;
